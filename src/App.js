1821b2a83d93698560a6603a0f8d20ab88b089db
Nsubuga Solomon <snsubuga@live.com>
2018-05-12 00:22:22 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">Book a meal</p>
+      </div>
+    );
+  }
+}
+
+export default App;
