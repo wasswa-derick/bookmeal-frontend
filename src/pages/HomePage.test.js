728c977184f08189a97409bf194d926cdeb0332c
Nsubuga Solomon <snsubuga@live.com>
2018-05-14 22:01:10 +0300

    feat: Add homepage

---
+++ src/pages/HomePage.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { shallow } from "enzyme";
+import HomePage from "./HomePage";
+import NavBar from "../components/NavBar";
+
+describe("HomePage", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<HomePage />);
+  });
+
+  it("should render a <div/>", () => {
+    expect(wrapper.find("div").length).toEqual(1);
+  });
+
+  it("should render a <div/>", () => {
+    expect(wrapper.containsMatchingElement(<NavBar />)).toEqual(true);
+  });
+});
