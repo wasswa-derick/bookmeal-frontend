90dee07981a656adcc77b7c441bb77f9ab82aa94
Nsubuga Solomon <snsubuga@live.com>
2018-05-16 02:05:55 +0300

    feat: Add routing to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NotFound from "./NotFound";
-import "./App.css";
+import "./css";
 import HomePage from "./pages/HomePage";
+import Signup from "./pages/Signup";
 
 const App = () => (
   <div>
     <Switch>
       <Route path="/" exact component={HomePage} />
+      <Route path="/signup" exact component={Signup} />
       <Route component={NotFound} />
-    </Switch>{" "}
+    </Switch>
   </div>
 );
 
