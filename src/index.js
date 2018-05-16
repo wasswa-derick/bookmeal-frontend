dc4aba4302e938819b147b6cc6e562cb00710bc6
Nsubuga Solomon <snsubuga@live.com>
2018-05-17 02:37:34 +0300

    chore: Add redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./App";
+import rootReducer from "./reducers";
 // import registerServiceWorker from "./registerServiceWorker";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
