50886c8ff77c4fccb8566d10d07f9005d9776bff
Nsubuga Solomon <snsubuga@live.com>
2018-05-17 03:35:05 +0300

    feat: Add action to create business accounts

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,20 @@
 import axios from "axios";
-import { USER_CREATED } from "../reducers/constants";
+import { USER_CREATED, CREATE_BUSINESS_ACCOUNT } from "../reducers/constants";
 
-export const createdUSer = data => ({
+export const createdUser = data => ({
   type: USER_CREATED,
   data
 });
 
+export const createdBusiness = data => ({
+  type: CREATE_BUSINESS_ACCOUNT,
+  data
+});
+
 export const registerCustomer = data => dispatch =>
-  axios.post("/auth/signup", data).then(res => dispatch(createdUSer(res.data)));
+  axios.post("/auth/signup", data).then(res => dispatch(createdUser(res.data)));
+
+export const registerBusiness = data => dispatch =>
+  axios
+    .post("/auth/business/signup", data)
+    .then(res => dispatch(createdBusiness(res.data)));
--- src/actions/auth.test.js
+++ src/actions/auth.test.js
@@ -1,7 +1,7 @@
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
 import * as actions from "./auth";
-import { USER_CREATED } from "../reducers/constants";
+import { USER_CREATED, CREATE_BUSINESS_ACCOUNT } from "../reducers/constants";
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
@@ -10,23 +10,35 @@ describe("authentication actions", () => {
   let data;
   let expectedActions;
   let userCreatedAction;
+  let businessData;
 
   beforeEach(() => {
     data = { email: "test@test.gmail", password: "test", name: "test" };
+    businessData = {
+      user: data,
+      business: { address: "Kla", name: "ADU" }
+    };
     userCreatedAction = {
       type: USER_CREATED,
       data
     };
-    expectedActions = [userCreatedAction];
+    expectedActions = [
+      userCreatedAction,
+      {
+        type: CREATE_BUSINESS_ACCOUNT,
+        data: { user: data, business: { address: "Kla", name: "ADU" } }
+      }
+    ];
   });
 
   it("should create an action to create a user", () => {
-    expect(actions.createdUSer(data)).toEqual(userCreatedAction);
+    expect(actions.createdUser(data)).toEqual(userCreatedAction);
   });
 
-  it("USER_CREATED is dispatched to redux store", () => {
+  it("authentication actions are dispatched to redux store", () => {
     const store = mockStore({ data: {} });
-    store.dispatch(actions.createdUSer(data));
+    store.dispatch(actions.createdUser(data));
+    store.dispatch(actions.createdBusiness(businessData));
     expect(store.getActions()).toEqual(expectedActions);
   });
 });
