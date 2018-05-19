0e6c5289f6bad20c2fd98a69f0aa98ad89394606
Nsubuga Solomon <snsubuga@live.com>
2018-05-20 00:43:19 +0300

    feat: Add user authentication.

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import LoginForm from "../components/forms/LoginForm";
+import { loginUser } from "../actions/auth";
+import NavBar from "../components/NavBar";
+/**
+ * @class LoginPage
+ * @extends {React.Component}
+ */
+export class LoginPage extends React.Component {
+  /**
+   * @param {object} data to submit
+   * @returns {Promise} promise to the caller
+   * @memberof LoginPage
+   */
+  handleSubmit = data =>
+    this.props.loginUser(data).then(() => this.props.history.push("/"));
+
+  /**
+   * @returns {any} renders a page
+   * @memberof LoginPage
+   */
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="container login-panel">
+          <div className="card auth-card">
+            <div className="card-body">
+              <h3 className="card-title title">Log Into Your Account</h3>
+              <LoginForm handleSubmit={this.handleSubmit} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+LoginPage.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired
+};
+export default connect(null, { loginUser })(LoginPage);
---
+++ src/pages/LoginPage.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { LoginPage } from "./LoginPage";
+import LoginForm from "../components/forms/LoginForm";
+
+describe("LoginPage", () => {
+  let wrapper;
+  beforeEach(() => {
+    const history = {
+      push: jest.fn
+    };
+    wrapper = shallow(<LoginPage loginUser={jest.fn} history={history} />);
+  });
+
+  it("should render a login form", () => {
+    const form = <LoginForm handleSubmit={wrapper.instance().handleSubmit} />;
+    expect(wrapper.containsMatchingElement(form)).toEqual(true);
+  });
+
+  it("should render correctly", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
