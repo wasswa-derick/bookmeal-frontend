5ccc7e38636c2b362693012942b135709091fbc9
Nsubuga Solomon <snsubuga@live.com>
2018-05-19 04:35:22 +0300

    chore: Add test for index app entry point

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+describe("index app entry point", () => {
+  it("renders without crashing", () => {
+    const root = document.createElement("div");
+    root.setAttribute("id", "root");
+    document.body.appendChild(root);
+    require("./index");
+  });
+});
