28a1768e5bf83f6e7f1b2483594f59904405fc37
Nsubuga Solomon <snsubuga@live.com>
2018-05-20 00:43:47 +0300

    feat: Add routes for login and user signup

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import { Route, Switch } from "react-router-dom";
 import NotFound from "./NotFound";
 import "./css";
 import HomePage from "./pages/HomePage";
-import Signup from "./pages/Signup";
+import ConnectedSignup from "./pages/Signup";
+import LoginPage from "./pages/LoginPage";
+import ConnectedBusinessSignupPage from "./pages/BusinessSignupPage";
 
 const App = () => (
   <div>
     <Switch>
       <Route path="/" exact component={HomePage} />
-      <Route path="/signup" exact component={Signup} />
+      <Route path="/signup" exact component={ConnectedSignup} />
+      <Route
+        path="/business/signup"
+        exact
+        component={ConnectedBusinessSignupPage}
+      />
+      <Route path="/login" exact component={LoginPage} />
       <Route component={NotFound} />
     </Switch>
   </div>
