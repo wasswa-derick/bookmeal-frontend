1c84a54b5e7be063d96943f234bdf75cf4b66308
Nsubuga Solomon <snsubuga@live.com>
2018-05-29 05:57:35 +0300

    feat: Add admin routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import PropTypes from "prop-types";
 import NotFound from "./NotFound";
 import "./css";
+import AppNavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import ConnectedSignup from "./pages/Signup";
-import LoginPage from "./pages/LoginPage";
+import ConnectedLoginPage from "./pages/LoginPage";
 import ConnectedBusinessSignupPage from "./pages/BusinessSignupPage";
+import AppGuestRoute from "./routes/GuestRoute";
+import AppAdminRoute from "./routes/AdminRoute";
+import AdminMealsPage from "./pages/admin/MealsPage";
 
-const App = () => (
+const App = ({ location }) => (
   <div>
+    <AppNavBar />
     <Switch>
       <Route path="/" exact component={HomePage} />
-      <Route path="/signup" exact component={ConnectedSignup} />
-      <Route
+      <AppGuestRoute path="/signup" exact component={ConnectedSignup} />
+      <AppGuestRoute
+        location={location}
         path="/business/signup"
         exact
         component={ConnectedBusinessSignupPage}
       />
-      <Route path="/login" exact component={LoginPage} />
+      <AppGuestRoute
+        location={location}
+        path="/login"
+        exact
+        component={ConnectedLoginPage}
+      />
+      <AppAdminRoute
+        location={location}
+        path="/admin/meals"
+        exact
+        component={AdminMealsPage}
+      />
       <Route component={NotFound} />
     </Switch>
   </div>
 );
+
+App.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  }).isRequired
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,12 @@ import App from "./App";
 
 describe("App Component", () => {
   it("renders  div", () => {
-    const wrapper = shallow(<App />);
+    const location = {
+      pathname: ""
+    };
+    const wrapper = shallow(<App location={location} />);
     expect(wrapper.find("div").length).toEqual(1);
   });
+
+  // TODO: add test for rendering correctly
 });
