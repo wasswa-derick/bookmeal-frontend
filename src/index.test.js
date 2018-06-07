12a839f436a977a12bb7ba617ed6829839d7d603
Nsubuga Solomon <snsubuga@live.com>
2018-06-07 15:41:45 +0300

    refactor test to mock local storage

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,23 @@
 /* eslint-disable */
+import mockLocalStorage from "./utils/localStorage";
+import jwt from "jsonwebtoken";
 
 describe("index app entry point", () => {
+  beforeEach(() => {
+    Object.defineProperty(window, "localStorage", {
+      value: mockLocalStorage
+    });
+
+    localStorage.setItem(
+      "authUserToken",
+      jwt.sign({ email: "test@test.com", name: "test" }, "secret")
+    );
+  });
+
   it("renders without crashing", () => {
     const root = document.createElement("div");
     root.setAttribute("id", "root");
     document.body.appendChild(root);
-    // require("./index");
+    require("./index");
   });
 });
