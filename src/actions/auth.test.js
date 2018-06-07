570b6e102d99b079770e3e61ea035b422d642df3
Nsubuga Solomon <snsubuga@live.com>
2018-06-07 03:26:47 +0300

    Add test for async auth actions

--- src/actions/auth.test.js
+++ src/actions/auth.test.js
@@ -1,5 +1,8 @@
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
+import moxios from "moxios";
+import jwt from "jsonwebtoken";
+import jwtDecode from "jwt-decode";
 import * as actions from "./auth";
 import {
   USER_CREATED,
@@ -7,56 +10,119 @@ import {
   USER_LOGGED_IN,
   USER_LOGGED_OUT
 } from "../reducers/constants";
+import mockLocalStorage from "../utils/localStorage";
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
+const registerCustomerMock = {
+  id: 1,
+  name: "test",
+  email: "test@test.com"
+};
+
+const registerBusinessMock = {
+  user: registerCustomerMock,
+  business: { address: "Kla", name: "ADU" }
+};
+
+const loginMock = {
+  token: jwt.sign({ email: "test@test.com", name: "test" }, "secret")
+};
+
 describe("authentication actions", () => {
   let data;
-  let expectedActions;
-  let userCreatedAction;
   let businessData;
 
   beforeEach(() => {
-    data = { email: "test@test.gmail", password: "test", name: "test" };
+    Object.defineProperty(window, "localStorage", {
+      value: mockLocalStorage
+    });
+
+    moxios.install();
+    data = { email: "test@test.com", password: "test", name: "test" };
     businessData = {
       user: data,
       business: { address: "Kla", name: "ADU" }
     };
-    userCreatedAction = {
-      type: USER_CREATED,
-      data
-    };
-    expectedActions = [
-      userCreatedAction,
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  it("creates USER_CREATED action after successfully registering a user", () => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: registerCustomerMock
+      });
+    });
+
+    const expectedAction = [
       {
-        type: CREATE_BUSINESS_ACCOUNT,
-        data: { user: data, business: { address: "Kla", name: "ADU" } }
-      },
-      {
-        type: USER_LOGGED_IN,
-        data: { email: "test@and.com", name: "test" }
-      },
-      {
-        type: USER_LOGGED_OUT
+        type: USER_CREATED,
+        data: registerCustomerMock
       }
     ];
-  });
 
-  it("should create an action to create a user", () => {
-    expect(actions.createdUser(data)).toEqual(userCreatedAction);
-  });
-
-  it("authentication actions are dispatched to redux store", () => {
     const store = mockStore({ data: {} });
-    store.dispatch(actions.createdUser(data));
-    store.dispatch(actions.createdBusiness(businessData));
-    store.dispatch(
-      actions.userLoggedIn({ email: "test@and.com", name: "test" })
-    );
-    store.dispatch(actions.userLoggedOut());
-    expect(store.getActions()).toEqual(expectedActions);
+    return store.dispatch(actions.registerCustomer(data)).then(() => {
+      expect(store.getActions()).toEqual(expectedAction);
+    });
   });
 
-  it("should perform async actions", () => {});
+  it("creates CREATED_BUSINESS_ACCOUNT action when a business is successfully registered", () => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: registerBusinessMock
+      });
+    });
+
+    const expectedAction = [
+      {
+        type: CREATE_BUSINESS_ACCOUNT,
+        data: registerBusinessMock
+      }
+    ];
+    const store = mockStore({ data: {} });
+    return store.dispatch(actions.registerBusiness(businessData)).then(() => {
+      expect(store.getActions()).toEqual(expectedAction);
+    });
+  });
+
+  it("creates USER_LOGGED_IN action on successfully logging in a user", () => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: loginMock
+      });
+    });
+
+    const user = jwtDecode(loginMock.token);
+
+    const expectedAction = [
+      {
+        type: USER_LOGGED_IN,
+        data: { ...user, isLoggedIn: true }
+      }
+    ];
+
+    const store = mockStore({ data: {} });
+    return store.dispatch(actions.loginUser(data)).then(() => {
+      expect(store.getActions()).toEqual(expectedAction);
+    });
+  });
+
+  it("should create USER_LOGGED_OUT action when user logs out", () => {
+    localStorage.setItem("authUserToken", loginMock.token);
+    const store = mockStore({});
+    const expectedAction = [{ type: USER_LOGGED_OUT }];
+    store.dispatch(actions.logoutUser());
+    expect(store.getActions()).toEqual(expectedAction);
+  });
 });
