209b2f53fb35dd5f6c99ebe72af253ac2f9436e2
Nsubuga Solomon <snsubuga@live.com>
2018-06-07 14:31:07 +0300

    refactor tests for aysnc menu actions

--- src/actions/menus.test.js
+++ src/actions/menus.test.js
@@ -1,52 +1,133 @@
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
+import moxios from "moxios";
 import * as actions from "./menus";
-import { FETCH_MENU, FETCH_MENUS, ADD_MENU } from "../reducers/constants";
+import {
+  FETCH_MENU,
+  FETCH_MENUS,
+  ADD_MENU,
+  FETCH_TODAY_MENUS
+} from "../reducers/constants";
+import mockLocalStorage from "../utils/localStorage";
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
+const postMenuMock = {
+  id: 1,
+  title: "title menu",
+  description: "menu desc",
+  meals: [
+    {
+      id: 1,
+      title: "title meal",
+      price: 1009
+    }
+  ]
+};
 
 describe("meals actions", () => {
   let data;
-  let expectedActions;
-
   beforeEach(() => {
+    Object.defineProperty(window, "localStorage", {
+      value: mockLocalStorage
+    });
+    moxios.install();
     data = {
-      id: 1,
       title: "test",
       description: "lorem description",
-      meals: [{ id: 1 }]
+      meals: [1]
     };
-
-    expectedActions = [
-      {
-        type: FETCH_MENU,
-        data
-      },
-      {
-        type: FETCH_MENUS,
-        data: [data]
-      },
-      {
-        type: ADD_MENU,
-        data
-      }
-    ];
   });
 
-  it("should create an action to add menu", () => {
-    expect(expectedActions[2]).toEqual({
-      type: ADD_MENU,
-      data
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  it("should create ADD_MENU action", () => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: postMenuMock
+      });
+    });
+
+    const expectedAction = [
+      {
+        type: ADD_MENU,
+        data: postMenuMock
+      }
+    ];
+
+    const store = mockStore({ data: {} });
+    return store.dispatch(actions.addMenu(data)).then(() => {
+      expect(store.getActions()).toEqual(expectedAction);
     });
   });
 
-  it("menu actions are dispatched to redux store", () => {
-    const store = mockStore({ data: {} });
-    store.dispatch(actions.gotMenu(data));
-    store.dispatch(actions.gotMenus([data]));
-    store.dispatch(actions.createdMenu(data));
+  it("should dispatch FETCH_MENU action", () => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: postMenuMock
+      });
+    });
 
-    expect(store.getActions()).toEqual(expectedActions);
+    const expectedAction = [
+      {
+        type: FETCH_MENU,
+        data: postMenuMock
+      }
+    ];
+
+    const store = mockStore({ data: {} });
+    return store.dispatch(actions.getMenu(1)).then(() => {
+      expect(store.getActions()).toEqual(expectedAction);
+    });
+  });
+
+  it("should dispatch FETCH_MENUS action", () => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: [postMenuMock]
+      });
+    });
+
+    const expectedAction = [
+      {
+        type: FETCH_MENUS,
+        data: [postMenuMock]
+      }
+    ];
+
+    const store = mockStore({ data: {} });
+    return store.dispatch(actions.getMenus()).then(() => {
+      expect(store.getActions()).toEqual(expectedAction);
+    });
+  });
+
+  it("should dispatch FETCH_TODAY_MENUS", () => {
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: [postMenuMock]
+      });
+    });
+
+    const expectedAction = [
+      {
+        type: FETCH_TODAY_MENUS,
+        data: [postMenuMock]
+      }
+    ];
+
+    const store = mockStore({ data: {} });
+    return store.dispatch(actions.getTodayMenus()).then(() => {
+      expect(store.getActions()).toEqual(expectedAction);
+    });
   });
 });
