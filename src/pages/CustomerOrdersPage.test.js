cd22b162e806a9f05f6d9e02929f986d65561c76
Nsubuga Solomon <snsubuga@live.com>
2018-06-08 14:32:56 +0300

    Refactored test

--- src/pages/CustomerOrdersPage.test.js
+++ src/pages/CustomerOrdersPage.test.js
@@ -8,6 +8,7 @@ const err = {
   }
 };
 const fn = () => Promise.reject(err);
+const asyncMockFn = () => Promise.resolve({});
 
 describe("CustomerOrdersPage", () => {
   let wrapper;
@@ -20,13 +21,19 @@ describe("CustomerOrdersPage", () => {
     };
     wrapper = shallow(
       <CustomerOrdersPage
-        getMyOrders={fn}
-        getMenu={fn}
+        getMyOrders={asyncMockFn}
+        getMenu={asyncMockFn}
         menu={menu}
         modifyOrder={fn}
         orders={[]}
       />
     );
+  });
+
+  it("should handle onSelected", () => {
+    const evt = {};
+    wrapper.instance().onSelected(evt, { id: 1 });
+    // console.log(wrapper.instance().state.selectedMenu);
   });
 
   it("should render correctly", () => {
