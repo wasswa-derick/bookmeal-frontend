e3b574737dc76746b28ff62e06e7ed914774f646
Nsubuga Solomon <snsubuga@live.com>
2018-08-30 12:42:29 +0300

    change to production api

--- src/actions/axiosInstance.js
+++ src/actions/axiosInstance.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5000/api/v1",
-  // baseURL: "https://mealbooking-api.herokuapp.com/api/v1/",
+  // baseURL: "http://127.0.0.1:5000/api/v1",
+  baseURL: "https://mealbooking-api.herokuapp.com/api/v1/",
   headers: { Authorization: `${localStorage.getItem("authUserToken")}` }
 });
 
 axiosInstance.interceptors.request.use(config => {
-  const token = localStorage.getItem("authUserToken")
+  const token = localStorage.getItem("authUserToken");
   if (token) {
     config.headers.Authorization = `${localStorage.getItem("authUserToken")}`;
   }
